7cf7e2208b621ab377d9f693446ac0fefdb89550
Darren Eberly <darren.eberly@proton.me>
2025-12-18 22:30:49 -0500

    Part 2 has movement now

--- code/Part 2/src/roguelike/Main.hx
+++ code/Part 2/src/roguelike/Main.hx
@@ -1,5 +1,6 @@
 package roguelike;
 
+import sdl.GameController;
 import h2d.Scene;
 
 import echoes.Echoes;
@@ -18,6 +19,14 @@ var FIELD_SIZE: Int;
         this.x = x;
         this.y = y;
     }
+}
+
+class GameState {
+
+    public var player: Entity;
+
+    public function new() {}
+
 }
 
 class RenderSystem extends System {
@@ -55,20 +64,23 @@ class RenderSystem extends System {
 
 class Main extends hxd.App {
 
+    public var state: GameState = new GameState();
+
     override function init(): Void {
         FIELD_SIZE = getFont().size;
         var window_width = FIELD_SIZE * SCREEN_WIDTH;
         var window_height = FIELD_SIZE * SCREEN_HEIGHT;
 
         hxd.Window.getInstance().resize(window_width, window_height);
+        hxd.Window.getInstance().addEventTarget(onEvent);
         
         new RenderSystem(this.s2d).activate();
 
-        var player: Entity = new Entity();
-        player.add(new h2d.Text(getFont()));
-        player.get(h2d.Text).text = "@";
-        player.get(h2d.Text).textColor = 0xFFFFFF00;
-        player.add(new Position(40, 25));
+        state.player = new Entity();
+        state.player.add(new h2d.Text(getFont()));
+        state.player.get(h2d.Text).text = "@";
+        state.player.get(h2d.Text).textColor = 0xFFFFFF00;
+        state.player.add(new Position(40, 25));
 
         for (i in 0...10) {
             var new_entity: Entity = new Entity();
@@ -77,6 +89,33 @@ class Main extends hxd.App {
             new_entity.get(h2d.Text).textColor = 0xFFFF0000;
             new_entity.add(new Position(i * 7, 20));
         }
+    }
+
+    function onEvent(event: hxd.Event) {
+        switch(event.kind) {
+            case EKeyDown:
+                onKeyDown(event.keyCode);
+            case _:
+        }
+    }
+
+    function onKeyDown(keyCode: Int) {
+        switch(keyCode) {
+            case hxd.Key.UP | hxd.Key.W:
+                movePlayer(0, -1);
+            case hxd.Key.DOWN | hxd.Key.S:
+                movePlayer(0, 1);
+            case hxd.Key.LEFT | hxd.Key.A:
+                movePlayer(-1, 0);
+            case hxd.Key.RIGHT | hxd.Key.D:
+                movePlayer(1, 0);
+        }
+    }
+
+    function movePlayer(deltaX: Int, deltaY: Int) {
+        var pos = this.state.player.get(Position);
+        pos.x = Std.int(Math.min(SCREEN_WIDTH - 1, Math.max(0, pos.x + deltaX)));
+        pos.y = Std.int(Math.min(SCREEN_HEIGHT - 1, Math.max(0, pos.y + deltaY)));
     }
 
     function getFont(): h2d.Font {
